33f68a22ebebfe42e5c16d209afb785a5cfececf
madssiggy <kanininaritainndahala@gmail.com>
2020-02-12 17:31:25 +0900

    AddStageAndExit

---
+++ New Unity Project (1)/Assets/Exit.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Exit : MonoBehaviour
+{
+    public bool isTouch;
+    public bool isClear;
+    private void Start()
+    {
+        isClear = false;
+        isTouch = false;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&&isClear==false) {
+            isTouch = true;
+            isClear = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            isTouch = false;
+            isClear = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "player") {
+
+        Destroy(GameObject.Find("Cube"));
+            Debug.Log("Destroyed");
+        }
+    }
+  public  bool GetIsTouch()
+    {
+        
+        return isTouch;
+    }
+    public bool GetisClear()
+    {
+        return isClear;
+    }
+}
